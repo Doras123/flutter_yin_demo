904494cbea0c01026fcfc73b0adc1864b2b22dcf
Doras123 <dokaixin70@gmail.com>
2021-08-17 01:17:41 +0800

    更新中

--- lib/view/sub_pages/home_pages/recommend_page.dart
+++ lib/view/sub_pages/home_pages/recommend_page.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_yin_demo/Components/rotation_chart.dart';
+
+final List<String> images = [
+  'https://img2.doubanio.com/view/music_topic_cover/large/public/topic-zhoubichanginterview-1.jpg',
+  'https://img2.doubanio.com/view/music_topic_cover/large/public/topic-nochoicewetware-1.jpg',
+  'https://img2.doubanio.com/view/music_topic_cover/large/public/topic-coldcavetour-2.jpg',
+  'https://img2.doubanio.com/view/music_topic_cover/large/public/topic-danielmiller-1.jpg',
+  'https://img2.doubanio.com/view/music_topic_cover/large/public/topic-murkycrowspainting-1.jpg',
+];
 
 class RecommendPage extends StatefulWidget {
   @override
@@ -8,8 +17,18 @@ class RecommendPage extends StatefulWidget {
 class _RecommendPageState extends State<RecommendPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Recommend Page')
+    return Column(
+      children:<Widget>[
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: 150,
+          margin: EdgeInsets.all(10),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: RotationChart(),
+          ),
+        )
+      ],
     );
   }
 }
